996486c3278289185227b129e5c3a44c77e32881
Alexis Vincent <alexisjohnvincent@gmail.com>
2015-05-24 23:46:13 +0200

    Added build dir

---
+++ build/gulpir.js
@@ -0,0 +1,26 @@
+"use strict";
+
+var _interopRequire = function (obj) { return obj && obj.__esModule ? obj["default"] : obj; };
+
+var gulpir = _interopRequire(require("laravel-elixir"));
+
+var util = require("gulp-util");
+
+gulpir.config.production = !!util.env.production;
+gulpir.config.srcDir = "/";
+gulpir.config.assetsDir = "/";
+gulpir.config.cssOutput = "build/css";
+gulpir.config.jsOutput = "build/js";
+gulpir.config.sourcemaps = !util.env.production;
+gulpir.config.bowerDir = "vendor/bower_components";
+gulpir.config.tasks = [];
+gulpir.config.watchers = { "default": {} };
+gulpir.config.duplicate = [];
+gulpir.config.concatenate = { css: [], js: [] };
+
+gulpir.config.setDefaultsFrom("gulper.json");
+
+//Extentions
+require("./ingredients/webpack");
+
+module.exports = gulpir;
