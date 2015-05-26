301500d5fb7763eb7aeebfe240896ce5b5cce9fb
Alexis Vincent <alexisjohnvincent@gmail.com>
2015-05-26 20:27:02 +0200

    Added syncDir & syncFile recipe

---
+++ src/ingredients/syncFile.js
@@ -0,0 +1,12 @@
+import elixir from 'laravel-elixir';
+
+elixir.extend("syncFile", function (src, dest) {
+
+	gulp.task('syncFile', function () {
+		return gulp.src(src).pipe(gulp.dest(dest));
+	});
+
+	this.registerWatcher("syncFile", src);
+
+	return this.queueTask("syncFile");
+});
